fc5758ceb7d16491922b46ede81c2f4916f7733c
José Flávio <joseflaviojr@outlook.com>
2017-06-09 01:37:49 -0300

    Nao está adicionando pt de leitura no bd

--- src/java/br/cesjf/lppo/servlets/AdicionaLeituraServlet.java
+++ src/java/br/cesjf/lppo/servlets/AdicionaLeituraServlet.java
@@ -1,13 +1,8 @@
 package br.cesjf.lppo.servlets;
 
-import br.cesjf.lppo.Coleta;
 import br.cesjf.lppo.Leitura;
 import br.cesjf.lppo.dao.ColetaDAO;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,16 +19,7 @@ public class AdicionaLeituraServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        List<Coleta> coletas = new ArrayList();
         
-        try {
-            ColetaDAO dao = new ColetaDAO();
-            coletas = dao.listAll();
-        } catch (Exception ex) {
-            Logger.getLogger(AdicionaLeituraServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        request.setAttribute("coletas", coletas);
         request.getRequestDispatcher("WEB-INF/criar-leitura.jsp").forward(request, response);
     }
     
@@ -41,14 +27,16 @@ public class AdicionaLeituraServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         Leitura leitura = new Leitura();
-        leitura.setColeta(Long.parseLong(request.getParameter("idColeta")));
         leitura.setUnidade(request.getParameter("unidade"));
         leitura.setLocalLeitura(request.getParameter("localLeitura"));
         try {
             ColetaDAO dao = new ColetaDAO();
             dao.criaPontoDeLeitura(leitura);
         } catch (Exception ex) {
-            Logger.getLogger(AdicionaLeituraServlet.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println(ex);
+            request.setAttribute("mensagem", ex);
+            request.getRequestDispatcher("WEB-INF/listar-coletas.jsp").forward(request, response);
+            return;
         }
         response.sendRedirect("listar-coletas.html");
     }
