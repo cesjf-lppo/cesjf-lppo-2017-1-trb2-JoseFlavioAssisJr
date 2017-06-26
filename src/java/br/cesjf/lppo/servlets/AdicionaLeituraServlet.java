91bcfa972ace71526fa3e81b9408eee436444165
José Flávio <joseflaviojr@outlook.com>
2017-06-26 00:36:02 -0300

    Leitura nao add no banco

--- src/java/br/cesjf/lppo/servlets/AdicionaLeituraServlet.java
+++ src/java/br/cesjf/lppo/servlets/AdicionaLeituraServlet.java
@@ -1,8 +1,13 @@
 package br.cesjf.lppo.servlets;
 
+import br.cesjf.lppo.Coleta;
 import br.cesjf.lppo.Leitura;
 import br.cesjf.lppo.dao.ColetaDAO;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,18 +23,28 @@ public class AdicionaLeituraServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, NumberFormatException {
-          
-        request.getRequestDispatcher("WEB-INF/criar-leitura.jsp").forward(request, response);
+            throws ServletException, IOException {
+        List<Coleta> coletas = new ArrayList();
+        
+        try {
+            ColetaDAO dao = new ColetaDAO();
+            coletas = dao.listAll();
+        } catch (Exception ex) {
+            Logger.getLogger(AdicionaLeituraServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        request.setAttribute("coletas", coletas);
+        
+        request.getRequestDispatcher("/WEB-INF/criar-leitura.jsp").forward(request, response);
     }
     
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, NumberFormatException {
+            throws ServletException, IOException{
+        
         Leitura leitura = new Leitura();
         leitura.setColeta(Long.parseLong(request.getParameter("id")));
-        leitura.setUnidade(request.getParameter("unidade"));
         leitura.setLocalLeitura(request.getParameter("localLeitura"));
+        leitura.setUnidade(request.getParameter("unidade"));
         try {
             ColetaDAO dao = new ColetaDAO();
             dao.criaPontoDeLeitura(leitura);
